dab2feffcae1331336284974c9b2d5595fb4cfd4
jk <joachim.dev@pm.me>
2024-01-02 19:56:06 +0100

    remove unnecessary bloat from kitty theme config

--- home-manager/features/kitty/default.nix
+++ home-manager/features/kitty/default.nix
@@ -1,34 +1,8 @@
 { config, ...}:
 
 let
-  kitty-theme = if config.darkmode then
-    ## Dark Themes
-     # "Ayu"
-     # "Dracula"
-     # "Gruvbox Material Dark Medium"
-     "Gruvbox Material Dark Hard"
-     # "Flatland"
-     # "Kaolin Aurora"
-     # "Kaolin Ocean"
-     # "Monokai"
-     # "Nord"
-     # "Nova"
-     # "Obsidian"
-     # "PaperColor Dark"
-     # "Obsidian"
-     # "Solarized Dark"
-     # "Spacemacs"
-     else
-    ## Light Themes
-    # "Ayu Light"
-    # "GitHub Light"
-    "Gruvbox Material Light Hard"
-    # "Material"
-    # "Piatto Light"
-    # "Solarized Light"
-    # "Tomorrow"
-    # "Spring"
-; in
+  kitty-theme = "Catppuccin-Mocha";
+in
 {
   programs.kitty = {
     enable = true;
