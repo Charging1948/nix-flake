3a242b4a1ee7202659c7796a1c60918b3e7ddf6c
jk <joachim.dev@pm.me>
2024-01-02 19:13:09 +0100

    update hyprland configuration

--- home-manager/features/hyprland.nix
+++ home-manager/features/hyprland.nix
@@ -35,6 +35,10 @@
     hypr_term = "kitty";
   in {
     enable = true;
+    package = pkgs.hyprland;
+    xwayland.enable = true;
+    systemd.enable = true;
+    enableNvidiaPatches = true;
     settings = {
       "$mod" = "SUPER";
       bind =
@@ -60,6 +64,42 @@
             )
             10)
         );
+      input = {
+        kb_layout = "de";
+        kb_variant = "nodeadkeys";
+
+        follow_mouse = 1;
+        accel_profile = "flat";
+        touchpad.scroll_factor = 0.1;
+      };
+
+      dwindle = {
+        # keep floating dimentions while tiling
+        pseudotile = true;
+        preserve_split = true;
+      };
+
+      misc = {
+        # disable auto polling for config file changes
+        disable_autoreload = true;
+
+        force_default_wallpaper = 0;
+
+        # disable dragging animation
+        animate_mouse_windowdragging = false;
+
+        # enable variable refresh rate (effective depending on hardware)
+        vrr = 1;
+      };
+
+      # touchpad gestures
+      gestures = {
+        workspace_swipe = true;
+        workspace_swipe_forever = true;
+      };
+
+      xwayland.force_zero_scaling = true;
+
     };
     plugins = with inputs.hyprland-plugins.packages.${pkgs.system}; [
       # borders-plus-plus
@@ -67,5 +107,14 @@
       hyprbars
       hyprwinwrap
     ];
+    extraConfig = ''
+    plugin {
+      csgo-vulkan-fix {
+        res_w = 1280
+        res_h = 800
+        class = cs2
+      }
+    }
+  '';
   };
 }
