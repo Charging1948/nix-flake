4265fc600dceeb21340e6e0c8ad743dd01be9b55
jk <joachim.dev@pm.me>
2024-01-20 03:07:13 +0100

    remove experimental flag from waybar

--- home-manager/features/wayland-wm/waybar.nix
+++ home-manager/features/wayland-wm/waybar.nix
@@ -46,9 +46,6 @@ in
   };
   programs.waybar = {
     enable = true;
-    package = pkgs.waybar.overrideAttrs (oa: {
-      mesonFlags = (oa.mesonFlags or  [ ]) ++ [ "-Dexperimental=true" ];
-    });
     systemd.enable = true;
     settings = {
       primary = {
