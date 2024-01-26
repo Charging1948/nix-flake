b2ef108208aafbea0ffd8bb57ef21074e2bd6d2a
jk <joachim.dev@pm.me>
2024-01-26 12:33:24 +0100

    add gamescope program enable for better support and add CAP_SYS_NICE capabilities for boosting process priority

--- nixos/modules/steam.nix
+++ nixos/modules/steam.nix
@@ -1,39 +1,46 @@
-{
-  inputs,
-  pkgs,
-  ...
+{ inputs
+, pkgs
+, ...
 }: {
   programs.steam = {
     enable = true;
     package = pkgs.steam.override {
-       extraPkgs = pkgs: with pkgs; [
-         gamescope
-         gamemode
-         mangohud
-         xorg.libXcursor
-         xorg.libXi
-         xorg.libXinerama
-         xorg.libXScrnSaver
-         libpng
-         libpulseaudio
-         libvorbis
-         stdenv.cc.cc.lib
-         libkrb5
-         keyutils
-       ];
-     };
+      extraPkgs = pkgs: with pkgs; [
+        unstable.gamescope
+        unstable.gamemode
+        unstable.mangohud
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXinerama
+        xorg.libXScrnSaver
+        libpng
+        libpulseaudio
+        libvorbis
+        stdenv.cc.cc.lib
+        libkrb5
+        keyutils
+      ];
+    };
     gamescopeSession.enable = true;
     extraCompatPackages = [
       inputs.nix-gaming.packages.${pkgs.system}.proton-ge
     ];
   };
-  programs.gamemode.enable = true;
+  programs.gamemode = {
+    enable = true;
+  };
+  programs.gamescope = {
+    enable = true;
+    package = pkgs.unstable.gamescope;
+    capSysNice = true;
+  };
   hardware.opengl = {
-    extraPackages = with pkgs; [mangohud];
-    extraPackages32 = with pkgs; [mangohud];
+    extraPackages = with pkgs.unstable; [ mangohud ];
+    extraPackages32 = with pkgs.unstable; [ mangohud ];
   };
   environment.systemPackages = with pkgs; [
     lutris
-    mangohud
+    unstable.mangohud
+    unstable.gamescope
   ];
 }
