b60508ae73a117380492d4a3fbd964a8d89c675a
jk <joachim.dev@pm.me>
2024-01-26 19:54:07 +0100

    add driSupport for 64bit and 32bit to opengl hardware

--- nixos/modules/steam.nix
+++ nixos/modules/steam.nix
@@ -1,40 +1,38 @@
-{ inputs
-, pkgs
-, ...
-}: {
+{ inputs, pkgs, ... }: {
   programs.steam = {
     enable = true;
     package = pkgs.steam.override {
-      extraPkgs = pkgs: with pkgs; [
-        unstable.gamescope
-        unstable.gamemode
-        unstable.mangohud
-        xorg.libXcursor
-        xorg.libXi
-        xorg.libXinerama
-        xorg.libXScrnSaver
-        libpng
-        libpulseaudio
-        libvorbis
-        stdenv.cc.cc.lib
-        libkrb5
-        keyutils
-      ];
+      extraPkgs = pkgs:
+        with pkgs; [
+          unstable.gamescope
+          unstable.gamemode
+          unstable.mangohud
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          libkrb5
+          keyutils
+        ];
     };
     gamescopeSession.enable = true;
-    extraCompatPackages = [
-      inputs.nix-gaming.packages.${pkgs.system}.proton-ge
-    ];
+    extraCompatPackages =
+      [ inputs.nix-gaming.packages.${pkgs.system}.proton-ge ];
   };
-  programs.gamemode = {
-    enable = true;
-  };
+  programs.gamemode = { enable = true; };
   programs.gamescope = {
     enable = true;
     package = pkgs.unstable.gamescope;
     capSysNice = true;
   };
   hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
     extraPackages = with pkgs.unstable; [ mangohud ];
     extraPackages32 = with pkgs.unstable; [ mangohud ];
   };
