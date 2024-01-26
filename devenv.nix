b93a308fe87d94ab5da7c242783ba1fdd4b578e3
jk <joachim.dev@pm.me>
2024-01-26 18:40:21 +0100

    add devenv to project

---
+++ devenv.nix
@@ -0,0 +1,9 @@
+{...}: {
+  # https://devenv.sh/languages/
+  languages.nix.enable = true;
+  languages.haskell.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  pre-commit.hooks.alejandra.enable = true;
+  pre-commit.hooks.deadnix.enable = true;
+}
