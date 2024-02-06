682110262bb8bf62c3962f1fd0d603029226c8a4
jk <joachim.dev@pm.me>
2024-02-06 15:25:05 +0100

    openrazer keep devices active on screensaver

--- nixos/modules/razer.nix
+++ nixos/modules/razer.nix
@@ -1,8 +1,8 @@
-{pkgs,...}:
-{
+{ pkgs, ... }: {
   boot.kernelParams = [ "button.lid_init_state=open" ];
   hardware.openrazer.enable = true;
-  hardware.openrazer.users = ["jk"];
+  hardware.openrazer.devicesOffOnScreensaver = false;
+  hardware.openrazer.users = [ "jk" ];
   environment.systemPackages = with pkgs.unstable; [
     openrazer-daemon
     polychromatic
