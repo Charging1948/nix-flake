1a96bfa4b10bd464f6295f5c3e6c4ce05641f4a0
jk <joachim.dev@pm.me>
2024-02-06 14:47:22 +0100

    add fancontrol

--- nixos/modules/hardware.nix
+++ nixos/modules/hardware.nix
@@ -1,14 +1,10 @@
-{ pkgs, ... }:
-{
-
+{ pkgs, ... }: {
   services.cpupower-gui.enable = true;
+  hardware.fancontrol.enable = true;
 
   services.hardware.bolt.enable = true;
   hardware.wooting.enable = true;
   hardware.keyboard.zsa.enable = true;
 
-  environment.systemPackages = with pkgs.unstable; [
-    keymapp
-    wootility
-  ];
+  environment.systemPackages = with pkgs.unstable; [ keymapp wootility ];
 }
