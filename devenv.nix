3aaa2d00b51161aac78c0ddf95964a1a76f9b3da
jk <joachim.dev@pm.me>
2024-02-06 15:29:36 +0100

    update pre-commit formatter

--- devenv.nix
+++ devenv.nix
@@ -1,10 +1,10 @@
-{ ... }: {
+{...}: {
   # https://devenv.sh/languages/
   languages.nix.enable = true;
   languages.haskell.enable = true;
 
   # https://devenv.sh/pre-commit-hooks/
-  pre-commit.hooks.nixfmt.enable = true;
+  pre-commit.hooks.alejandra.enable = true;
   pre-commit.hooks.nil.enable = true;
   # pre-commit.hooks.deadnix.enable = true;
 }
