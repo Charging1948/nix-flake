b6016d44f7c698942d9e7d9f142ca5f6f5ca055f
jk <joachim.dev@pm.me>
2024-02-07 13:13:59 +0100

    change jetbrains ides to unstable versions

--- nixos/modules/development.nix
+++ nixos/modules/development.nix
@@ -1,20 +1,29 @@
-{ environment, pkgs, lib, ... }:
-let jetbrains-idea-config = {
-  programs = [
-    "clion"
-    "webstorm"
-    "rust-rover"
-    "pycharm-professional"
-    "rider"
-    "idea-ultimate"
-    "goland"
-  ];
-  extensions = [
-    "ideavim"
-    "github-copilot"
-  ];
-};
-in 
-  {
-    environment.systemPackages = map (pkgName: lib.attrByPath [ pkgName ] null pkgs.jetbrains // pkgs.jetbrains.plugins.addPlugins pkgs.jetbrains.${pkgName} jetbrains-idea-config.extensions) jetbrains-idea-config.programs;
-  }
+{
+  environment,
+  pkgs,
+  lib,
+  ...
+}: let
+  jetbrains-idea-config = {
+    programs = [
+      "clion"
+      "webstorm"
+      "rust-rover"
+      "pycharm-professional"
+      "rider"
+      "idea-ultimate"
+      "goland"
+    ];
+    extensions = ["ideavim" "github-copilot"];
+  };
+in {
+  environment.systemPackages = let
+    inherit (pkgs.unstable) jetbrains;
+  in
+    map
+    (pkgName:
+      lib.attrByPath [pkgName] null jetbrains
+      // jetbrains.plugins.addPlugins jetbrains.${pkgName}
+      jetbrains-idea-config.extensions)
+    jetbrains-idea-config.programs;
+}
