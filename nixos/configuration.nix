a5f8f88782de405a842435933eb5c8e1c83bade5
jk <joachim.dev@pm.me>
2024-02-12 07:59:48 +0100

    update base config, add automatic garbage collection and limit boot generations to 25

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,6 +1,13 @@
 # This is your system's configuration file.
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
-{ inputs, outputs, lib, config, pkgs, ... }: {
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
   # You can import other NixOS modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/nixos):
@@ -49,16 +56,20 @@
 
   # This will add each flake input as a registry
   # To make nix3 commands consistent with your flake
-  nix.registry = (lib.mapAttrs (_: flake: { inherit flake; }))
+  nix.registry =
+    (lib.mapAttrs (_: flake: {inherit flake;}))
     ((lib.filterAttrs (_: lib.isType "flake")) inputs);
 
   # This will additionally add your inputs to the system's legacy channels
   # Making legacy nix commands consistent as well, awesome!
-  nix.nixPath = [ "/etc/nix/path" ];
-  environment.etc = lib.mapAttrs' (name: value: {
-    name = "nix/path/${name}";
-    value.source = value.flake;
-  }) config.nix.registry;
+  nix.nixPath = ["/etc/nix/path"];
+  environment.etc =
+    lib.mapAttrs'
+    (name: value: {
+      name = "nix/path/${name}";
+      value.source = value.flake;
+    })
+    config.nix.registry;
 
   nix.settings = {
     # Enable flakes and new 'nix' command
@@ -77,12 +88,21 @@
     ];
   };
 
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
   # TODO: Set your hostname
   networking.hostName = "protean";
 
   # TODO: This is just an example, be sure to use whatever bootloader you prefer
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+    systemd-boot.configurationLimit = 25;
+  };
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -106,21 +126,20 @@
   };
 
   # Add Fonts
-  fonts.packages = with pkgs;
-    [
-      (nerdfonts.override {
-        fonts = [
-          "FiraCode"
-          "CascadiaCode"
-          "IBMPlexMono"
-          "Iosevka"
-          "IosevkaTerm"
-          "JetBrainsMono"
-          "RobotoMono"
-          "Terminus"
-        ];
-      })
-    ];
+  fonts.packages = with pkgs; [
+    (nerdfonts.override {
+      fonts = [
+        "FiraCode"
+        "CascadiaCode"
+        "IBMPlexMono"
+        "Iosevka"
+        "IosevkaTerm"
+        "JetBrainsMono"
+        "RobotoMono"
+        "Terminus"
+      ];
+    })
+  ];
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
@@ -149,7 +168,7 @@
     inputs.nix-gaming.packages.${pkgs.system}.rocket-league
   ];
 
-  hardware = { opengl.enable = true; };
+  hardware = {opengl.enable = true;};
 
   # Configure keymap in X11
   services.xserver = {
@@ -201,12 +220,12 @@
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
       # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = [ "wheel" ];
+      extraGroups = ["wheel"];
       useDefaultShell = true;
     };
   };
 
-  environment.pathsToLink = [ "/share/zsh" ];
+  environment.pathsToLink = ["/share/zsh"];
 
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
