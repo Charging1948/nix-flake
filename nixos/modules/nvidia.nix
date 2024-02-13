f0f8dc239ec525da6a9701f708dd6d7af09378af
jk <joachim.dev@pm.me>
2024-02-13 07:56:57 +0100

    revert back to nvidia reversesync to test performance

--- nixos/modules/nvidia.nix
+++ nixos/modules/nvidia.nix
@@ -1,33 +1,40 @@
-{ pkgs, lib, inputs, ... }: {
-  boot.kernelParams = [ "nvidia_drm.modeset=1" ];
+{
+  pkgs,
+  lib,
+  inputs,
+  ...
+}: {
+  boot.kernelParams = ["nvidia_drm.modeset=1"];
+  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_zen;
   hardware.nvidia = {
     modesetting.enable = lib.mkDefault true;
     open = lib.mkForce false;
     powerManagement.enable = lib.mkDefault true;
     powerManagement.finegrained = lib.mkDefault false;
     prime = {
-      offload = let mkOptional = lib.mkOverride 500;
+      offload = let
+        mkOptional = lib.mkOverride 500;
       in {
-        enable = mkOptional true;
-        enableOffloadCmd = mkOptional true;
+        enable = mkOptional false;
+        enableOffloadCmd = mkOptional false;
       };
-      reverseSync.enable = lib.mkDefault false;
+      reverseSync.enable = lib.mkDefault true;
       sync.enable = lib.mkDefault false;
-      allowExternalGpu = lib.mkDefault false;
+      allowExternalGpu = lib.mkDefault true;
       intelBusId = lib.mkDefault "PCI:0:2:0";
       nvidiaBusId = lib.mkDefault "PCI:1:0:0";
     };
   };
   services.thermald.enable = lib.mkDefault false;
 
-  environment.systemPackages = with pkgs; [ nvidia-offload nvtop glxinfo ];
+  environment.systemPackages = with pkgs; [nvidia-offload nvtop glxinfo];
 
   specialisation = {
-
     # Enable Sync Mode for maximum performance
-    high-performance.configuration = let mkAlmostForce = lib.mkOverride 102;
+    high-performance.configuration = let
+      mkAlmostForce = lib.mkOverride 102;
     in {
-      system.nixos.tags = [ "high-performance" ];
+      system.nixos.tags = ["high-performance"];
 
       boot.kernelPackages = mkAlmostForce pkgs.linuxPackages_xanmod;
       hardware.nvidia.prime = {
@@ -42,9 +49,8 @@
 
     # Completely disable nvidia card to save battery.
     on-the-go.configuration = {
-      system.nixos.tags = [ "on-the-go" ];
-      imports = with inputs.nixos-hardware.nixosModules;
-        [ common-gpu-nvidia-disable ];
+      system.nixos.tags = ["on-the-go"];
+      imports = with inputs.nixos-hardware.nixosModules; [common-gpu-nvidia-disable];
 
       boot.kernelPackages = lib.mkForce pkgs.linuxPackages_zen;
       services.thermald.enable = lib.mkForce true;
