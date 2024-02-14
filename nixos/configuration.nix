7e082d952b7e0d958dc53bf14b9ac08a8b95bfe3
jk <joachim.dev@pm.me>
2024-02-14 11:37:57 +0100

    add nixpkgs-wayland binar cache to configuration and use its overlay

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -37,6 +37,7 @@
       outputs.overlays.modifications
       outputs.overlays.unstable-packages
 
+      inputs.nixpkgs-wayland.overlay
       # You can also add overlays exported from other flakes:
       # neovim-nightly-overlay.overlays.default
 
@@ -80,11 +81,15 @@
       "https://nix-gaming.cachix.org"
       "https://devenv.cachix.org"
       "https://hyprland.cachix.org"
+      "https://cache.nixos.org"
+      "https://nixpkgs-wayland.cachix.org"
     ];
     trusted-public-keys = [
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
     ];
   };
 
