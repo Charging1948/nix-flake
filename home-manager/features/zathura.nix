7b51391ede8ecca5421b8ab7ee129c940bebcb1f
jk <joachim.dev@pm.me>
2024-02-14 11:49:46 +0100

    fix last occurence of colors option

--- home-manager/features/zathura.nix
+++ home-manager/features/zathura.nix
@@ -1,32 +1,30 @@
-{ config, ... }:
-let inherit (config.colorScheme) colors;
-in {
+{config, ...}: {
   programs.zathura = {
     enable = true;
-    options = {
+    options = with config.colorScheme.palette; {
       selection-clipboard = "clipboard";
       # font = "${config.fontProfiles.regular.family} 12";
       recolor = true;
-      default-bg = "#${colors.base00}";
-      default-fg = "#${colors.base01}";
-      statusbar-bg = "#${colors.base02}";
-      statusbar-fg = "#${colors.base04}";
-      inputbar-bg = "#${colors.base00}";
-      inputbar-fg = "#${colors.base07}";
-      notification-bg = "#${colors.base00}";
-      notification-fg = "#${colors.base07}";
-      notification-error-bg = "#${colors.base00}";
-      notification-error-fg = "#${colors.base08}";
-      notification-warning-bg = "#${colors.base00}";
-      notification-warning-fg = "#${colors.base08}";
-      highlight-color = "#${colors.base0A}";
-      highlight-active-color = "#${colors.base0D}";
-      completion-bg = "#${colors.base01}";
-      completion-fg = "#${colors.base05}";
-      completions-highlight-bg = "#${colors.base0D}";
-      completions-highlight-fg = "#${colors.base07}";
-      recolor-lightcolor = "#${colors.base00}";
-      recolor-darkcolor = "#${colors.base06}";
+      default-bg = "#${base00}";
+      default-fg = "#${base01}";
+      statusbar-bg = "#${base02}";
+      statusbar-fg = "#${base04}";
+      inputbar-bg = "#${base00}";
+      inputbar-fg = "#${base07}";
+      notification-bg = "#${base00}";
+      notification-fg = "#${base07}";
+      notification-error-bg = "#${base00}";
+      notification-error-fg = "#${base08}";
+      notification-warning-bg = "#${base00}";
+      notification-warning-fg = "#${base08}";
+      highlight-color = "#${base0A}";
+      highlight-active-color = "#${base0D}";
+      completion-bg = "#${base01}";
+      completion-fg = "#${base05}";
+      completions-highlight-bg = "#${base0D}";
+      completions-highlight-fg = "#${base07}";
+      recolor-lightcolor = "#${base00}";
+      recolor-darkcolor = "#${base06}";
     };
   };
 }
