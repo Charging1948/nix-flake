4e5698d9e5fd6e3263e01e6dd08d2e7a343a8097
jk <joachim.dev@pm.me>
2024-02-20 02:55:55 +0100

    add icons module

---
+++ modules/home-manager/icons.nix
@@ -0,0 +1,17 @@
+{
+  pkgs,
+  inputs,
+  lib,
+  config,
+  ...
+}: let
+  inherit (lib) types mkOption mkEnableOption;
+in {
+  options.icons = mkOption {
+    type = types.path;
+    default = let
+      wallpaperPackage = inputs.md-icons;
+    in "${wallpaperPackage}/svg";
+    description = "Base path of icons file. This can be overridden by the user.";
+  };
+}
