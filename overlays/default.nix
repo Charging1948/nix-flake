5e34d6950acefb96d1ff79f611939bdead8c637e
jk <joachim.dev@pm.me>
2024-03-16 16:18:56 +0100

    try override in overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -11,6 +11,16 @@
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
   # be accessible through 'pkgs.unstable'
   unstable-packages = final: _prev: rec {
+    final =
+      _prev
+      // {
+        pkgs = import inputs.nixpkgs {
+          system = final.system;
+          config.allowUnfree = true;
+          config.permittedInsecurePackages = ["nix-2.16.2"];
+        };
+      };
+
     unstable = import inputs.nixpkgs-unstable {
       system = final.system;
       config.allowUnfree = true;
